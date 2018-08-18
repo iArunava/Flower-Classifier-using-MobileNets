abba54d70661d681a5d756653d4cad165db126aa
Arunava <learningdroidarunava@gmail.com>
2018-08-18 22:59:13 +0530

    Added init file to start webcam and make predictions and modified label_image.py to the needs of the project

---
+++ init.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.insert(0, './scripts/')
+
+import cv2 as cv
+import subprocess
+from label_image import *
+
+model_file = './tf_files/retrained_graph.pb'
+label_file = './tf_files/retrained_labels.txt'
+input_layer = 'input'
+output_layer = 'final_result'
+
+input_name = 'import/' + input_layer
+output_name = 'import/' + output_layer
+
+graph = load_graph(model_file)
+input_op = graph.get_operation_by_name(input_name)
+output_op = graph.get_operation_by_name(output_name)
+labels = load_labels(label_file)
+
+font = cv.FONT_HERSHEY_COMPLEX
+
+def predict_from_cam():
+    vid = cv.VideoCapture(0)
+
+    while True:
+        _, frame = vid.read()
+        preds = prediction(frame, graph, labels, input_op, output_op)
+
+        for i, pred in enumerate(preds):
+            cv.putText(frame, str(pred[0]) + ' : ' + str(pred[1]), (10, 80+(20*i)), font, 0.7, (255, 255, 0))
+
+        cv.imshow('Flower Classifier Cam', frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    vid.release()
+    cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    predict_from_cam()
+
--- scripts/label_image.py
+++ scripts/label_image.py
@@ -1,17 +1,5 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+# The following code is taken from https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/label_image/label_image.py 
+# and is modified to the needs of the project
 
 from __future__ import absolute_import
 from __future__ import division
@@ -19,7 +7,6 @@ from __future__ import print_function
 
 import argparse
 import sys
-import time
 
 import numpy as np
 import tensorflow as tf
@@ -35,23 +22,9 @@ def load_graph(model_file):
 
   return graph
 
-def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
+def read_tensor_from_image_file(image, input_height=299, input_width=299,
 				input_mean=0, input_std=255):
-  input_name = "file_reader"
-  output_name = "normalized"
-  file_reader = tf.read_file(file_name, input_name)
-  if file_name.endswith(".png"):
-    image_reader = tf.image.decode_png(file_reader, channels = 3,
-                                       name='png_reader')
-  elif file_name.endswith(".gif"):
-    image_reader = tf.squeeze(tf.image.decode_gif(file_reader,
-                                                  name='gif_reader'))
-  elif file_name.endswith(".bmp"):
-    image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
-  else:
-    image_reader = tf.image.decode_jpeg(file_reader, channels = 3,
-                                        name='jpeg_reader')
-  float_caster = tf.cast(image_reader, tf.float32)
+  float_caster = tf.cast(image, tf.float32)
   dims_expander = tf.expand_dims(float_caster, 0);
   resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
   normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
@@ -67,71 +40,27 @@ def load_labels(label_file):
     label.append(l.rstrip())
   return label
 
-if __name__ == "__main__":
-  file_name = "tf_files/flower_photos/daisy/3475870145_685a19116d.jpg"
-  model_file = "tf_files/retrained_graph.pb"
-  label_file = "tf_files/retrained_labels.txt"
+def prediction(image, graph, labels, input_operation, output_operation):
   input_height = 224
   input_width = 224
   input_mean = 128
   input_std = 128
-  input_layer = "input"
-  output_layer = "final_result"
-
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--image", help="image to be processed")
-  parser.add_argument("--graph", help="graph/model to be executed")
-  parser.add_argument("--labels", help="name of file containing labels")
-  parser.add_argument("--input_height", type=int, help="input height")
-  parser.add_argument("--input_width", type=int, help="input width")
-  parser.add_argument("--input_mean", type=int, help="input mean")
-  parser.add_argument("--input_std", type=int, help="input std")
-  parser.add_argument("--input_layer", help="name of input layer")
-  parser.add_argument("--output_layer", help="name of output layer")
-  args = parser.parse_args()
-
-  if args.graph:
-    model_file = args.graph
-  if args.image:
-    file_name = args.image
-  if args.labels:
-    label_file = args.labels
-  if args.input_height:
-    input_height = args.input_height
-  if args.input_width:
-    input_width = args.input_width
-  if args.input_mean:
-    input_mean = args.input_mean
-  if args.input_std:
-    input_std = args.input_std
-  if args.input_layer:
-    input_layer = args.input_layer
-  if args.output_layer:
-    output_layer = args.output_layer
-
-  graph = load_graph(model_file)
-  t = read_tensor_from_image_file(file_name,
+  
+  t = read_tensor_from_image_file(image,
                                   input_height=input_height,
                                   input_width=input_width,
                                   input_mean=input_mean,
                                   input_std=input_std)
 
-  input_name = "import/" + input_layer
-  output_name = "import/" + output_layer
-  input_operation = graph.get_operation_by_name(input_name);
-  output_operation = graph.get_operation_by_name(output_name);
-
   with tf.Session(graph=graph) as sess:
-    start = time.time()
     results = sess.run(output_operation.outputs[0],
                       {input_operation.outputs[0]: t})
-    end=time.time()
   results = np.squeeze(results)
 
   top_k = results.argsort()[-5:][::-1]
-  labels = load_labels(label_file)
-
-  print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
-  template = "{} (score={:0.5f})"
+  
+  preds = []
   for i in top_k:
-    print(template.format(labels[i], results[i]))
+    preds.append((labels[i], results[i]))
+
+  return preds
