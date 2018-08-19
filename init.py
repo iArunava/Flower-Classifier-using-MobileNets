084549783bb7e9c0e6ec048a890e4562fe476cd7
Arunava <learningdroidarunava@gmail.com>
2018-08-19 22:42:36 +0530

    Removed unecessary imports

--- init.py
+++ init.py
@@ -2,11 +2,10 @@ import sys
 sys.path.insert(0, './scripts/')
 
 import cv2 as cv
-import subprocess
 from label_image import *
 
-model_file = './tf_files/retrained_graph.pb'
-label_file = './tf_files/retrained_labels.txt'
+model_file = './tf_files/mobilenet_1.0_224_graph.pb'
+label_file = './tf_files/mobilenet_1.0_224_labels.txt'
 input_layer = 'input'
 output_layer = 'final_result'
 
@@ -28,16 +27,16 @@ def predict_from_cam():
         preds = prediction(frame, graph, labels, input_op, output_op)
 
         for i, pred in enumerate(preds):
-            cv.putText(frame, str(pred[0]) + ' : ' + str(pred[1]), (10, 80+(20*i)), font, 0.7, (255, 255, 0))
+            cv.putText(frame, str(pred[0]) + ' : ' + str(pred[1]),
+                       (10, 80+(20*i)), font, 0.7, (0, 0, 255))
 
         cv.imshow('Flower Classifier Cam', frame)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
-    
+
     vid.release()
     cv.destroyAllWindows()
 
 
 if __name__ == '__main__':
     predict_from_cam()
-
